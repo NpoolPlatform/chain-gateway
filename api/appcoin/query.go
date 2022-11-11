591fbfc875220056ce9e32a9f22a195daa5517a1
Zhao KK <kikakkz@hotmail.com>
2022-11-11 14:23:26 +0000

    Check uuid before use oit

--- api/appcoin/query.go
+++ api/appcoin/query.go
@@ -4,9 +4,8 @@ package appcoin
 import (
 	"context"
 
-	commontracer "github.com/NpoolPlatform/chain-gateway/pkg/tracer"
-
 	constant "github.com/NpoolPlatform/chain-gateway/pkg/message/const"
+	commontracer "github.com/NpoolPlatform/chain-gateway/pkg/tracer"
 
 	appcoinmwcli "github.com/NpoolPlatform/chain-middleware/pkg/client/appcoin"
 
@@ -19,7 +18,10 @@ import (
 	npool "github.com/NpoolPlatform/message/npool/chain/gw/v1/appcoin"
 	appcoinmwpb "github.com/NpoolPlatform/message/npool/chain/mw/v1/appcoin"
 
+	"github.com/NpoolPlatform/go-service-framework/pkg/logger"
 	cruder "github.com/NpoolPlatform/libent-cruder/pkg/cruder"
+
+	"github.com/google/uuid"
 )
 
 func (s *Server) GetCoins(ctx context.Context, in *npool.GetCoinsRequest) (*npool.GetCoinsResponse, error) {
@@ -38,6 +40,10 @@ func (s *Server) GetCoins(ctx context.Context, in *npool.GetCoinsRequest) (*npoo
 	span = commontracer.TraceOffsetLimit(span, int(in.GetOffset()), int(in.GetLimit()))
 	span = commontracer.TraceInvoker(span, "coin", "coin", "Rows")
 
+	if _, err := uuid.Parse(in.GetAppID()); err != nil {
+		return &npool.GetCoinsResponse{}, status.Error(codes.InvalidArgument, err.Error())
+	}
+
 	infos, total, err := appcoinmwcli.GetCoins(ctx, &appcoinmwpb.Conds{
 		AppID: &commonpb.StringVal{
 			Op:    cruder.EQ,
@@ -45,6 +51,7 @@ func (s *Server) GetCoins(ctx context.Context, in *npool.GetCoinsRequest) (*npoo
 		},
 	}, in.GetOffset(), in.GetLimit())
 	if err != nil {
+		logger.Sugar().Errorw("GetCoins", "error", err)
 		return &npool.GetCoinsResponse{}, status.Error(codes.Internal, err.Error())
 	}
 
