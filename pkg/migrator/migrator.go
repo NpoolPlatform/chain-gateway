eb53e009a5122b9c7d5ca2c38028a7080e9fc381
Zhao KK <kikakkz@hotmail.com>
2023-01-11 15:46:48 +0000

    Remove migration

--- pkg/migrator/migrator.go
+++ pkg/migrator/migrator.go
@@ -3,38 +3,8 @@ package migrator
 
 import (
 	"context"
-
-	"github.com/NpoolPlatform/chain-manager/pkg/db"
-	"github.com/NpoolPlatform/chain-manager/pkg/db/ent"
-
-	"github.com/shopspring/decimal"
 )
 
 func Migrate(ctx context.Context) error {
-	if err := db.Init(); err != nil {
-		return err
-	}
-
-	return db.WithClient(ctx, func(_ctx context.Context, cli *ent.Client) error {
-		_, err := cli.
-			AppCoin.
-			Update().
-			SetMaxAmountPerWithdraw(decimal.NewFromInt(40000)). //nolint
-			SetDailyRewardAmount(decimal.NewFromInt(0)).
-			Save(_ctx)
-		if err != nil {
-			return err
-		}
-
-		_, err = cli.
-			Setting.
-			Update().
-			SetLeastTransferAmount(decimal.RequireFromString("0.1")). //nolint
-			Save(_ctx)
-		if err != nil {
-			return err
-		}
-
-		return nil
-	})
+	return nil
 }
