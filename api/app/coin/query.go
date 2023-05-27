3cfd35802c49831c32f0324bcceb627cccab52c7
Zhao KK <kikakkz@hotmail.com>
2023-05-27 13:11:55 +0000

    Add delete and query apis

--- api/app/coin/query.go
+++ api/app/coin/query.go
@@ -4,43 +4,37 @@ package appcoin
 import (
 	"context"
 
-	constant "github.com/NpoolPlatform/chain-gateway/pkg/message/const"
-	commontracer "github.com/NpoolPlatform/chain-gateway/pkg/tracer"
+	appcoin1 "github.com/NpoolPlatform/chain-gateway/pkg/app/coin"
+	"github.com/NpoolPlatform/go-service-framework/pkg/logger"
+	npool "github.com/NpoolPlatform/message/npool/chain/gw/v1/app/coin"
 
-	"go.opentelemetry.io/otel"
-	scodes "go.opentelemetry.io/otel/codes"
 	"google.golang.org/grpc/codes"
 	"google.golang.org/grpc/status"
-
-	appcoin1 "github.com/NpoolPlatform/chain-gateway/pkg/appcoin"
-	"github.com/NpoolPlatform/go-service-framework/pkg/logger"
-	npool "github.com/NpoolPlatform/message/npool/chain/gw/v1/appcoin"
-	"github.com/google/uuid"
 )
 
 func (s *Server) GetCoins(ctx context.Context, in *npool.GetCoinsRequest) (*npool.GetCoinsResponse, error) {
-	var err error
-
-	_, span := otel.Tracer(constant.ServiceName).Start(ctx, "GetCoins")
-	defer span.End()
-
-	defer func() {
-		if err != nil {
-			span.SetStatus(scodes.Error, err.Error())
-			span.RecordError(err)
-		}
-	}()
-
-	span = commontracer.TraceOffsetLimit(span, int(in.GetOffset()), int(in.GetLimit()))
-	span = commontracer.TraceInvoker(span, "coin", "coin", "Rows")
-
-	if _, err := uuid.Parse(in.GetAppID()); err != nil {
+	handler, err := appcoin1.NewHandler(
+		ctx,
+		appcoin1.WithAppID(&in.AppID),
+		appcoin1.WithOffset(in.GetOffset()),
+		appcoin1.WithLimit(in.GetLimit()),
+	)
+	if err != nil {
+		logger.Sugar().Errorw(
+			"GetCoins",
+			"In", in,
+			"Error", err,
+		)
 		return &npool.GetCoinsResponse{}, status.Error(codes.InvalidArgument, err.Error())
 	}
 
-	infos, total, err := appcoin1.GetAppCoins(ctx, in.GetAppID(), in.GetOffset(), in.GetLimit())
+	infos, total, err := handler.GetCoins(ctx)
 	if err != nil {
-		logger.Sugar().Errorw("GetCoins", "error", err)
+		logger.Sugar().Errorw(
+			"GetCoins",
+			"In", in,
+			"Error", err,
+		)
 		return &npool.GetCoinsResponse{}, status.Error(codes.Internal, err.Error())
 	}
 
@@ -51,28 +45,28 @@ func (s *Server) GetCoins(ctx context.Context, in *npool.GetCoinsRequest) (*npoo
 }
 
 func (s *Server) GetAppCoins(ctx context.Context, in *npool.GetAppCoinsRequest) (*npool.GetAppCoinsResponse, error) {
-	var err error
-
-	_, span := otel.Tracer(constant.ServiceName).Start(ctx, "GetAppCoins")
-	defer span.End()
-
-	defer func() {
-		if err != nil {
-			span.SetStatus(scodes.Error, err.Error())
-			span.RecordError(err)
-		}
-	}()
-
-	span = commontracer.TraceOffsetLimit(span, int(in.GetOffset()), int(in.GetLimit()))
-	span = commontracer.TraceInvoker(span, "coin", "coin", "Rows")
-
-	if _, err := uuid.Parse(in.GetTargetAppID()); err != nil {
+	handler, err := appcoin1.NewHandler(
+		ctx,
+		appcoin1.WithAppID(&in.TargetAppID),
+		appcoin1.WithOffset(in.GetOffset()),
+		appcoin1.WithLimit(in.GetLimit()),
+	)
+	if err != nil {
+		logger.Sugar().Errorw(
+			"GetAppCoins",
+			"In", in,
+			"Error", err,
+		)
 		return &npool.GetAppCoinsResponse{}, status.Error(codes.InvalidArgument, err.Error())
 	}
 
-	infos, total, err := appcoin1.GetAppCoins(ctx, in.GetTargetAppID(), in.GetOffset(), in.GetLimit())
+	infos, total, err := handler.GetCoins(ctx)
 	if err != nil {
-		logger.Sugar().Errorw("GetAppCoins", "error", err)
+		logger.Sugar().Errorw(
+			"GetAppCoins",
+			"In", in,
+			"Error", err,
+		)
 		return &npool.GetAppCoinsResponse{}, status.Error(codes.Internal, err.Error())
 	}
 
