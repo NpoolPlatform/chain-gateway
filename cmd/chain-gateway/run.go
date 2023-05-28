862e50b9f54b2334173d836f208024e00c8f31c4
Zhao KK <kikakkz@hotmail.com>
2023-05-28 11:48:16 +0000

    Use action funciton

--- cmd/chain-gateway/run.go
+++ cmd/chain-gateway/run.go
@@ -1,13 +1,13 @@
 package main
 
 import (
+	"context"
+
 	"github.com/NpoolPlatform/chain-gateway/api"
 	"github.com/NpoolPlatform/chain-gateway/pkg/migrator"
 
-	grpc2 "github.com/NpoolPlatform/go-service-framework/pkg/grpc"
-	"github.com/NpoolPlatform/go-service-framework/pkg/logger"
-
 	apicli "github.com/NpoolPlatform/basal-middleware/pkg/client/api"
+	"github.com/NpoolPlatform/go-service-framework/pkg/action"
 
 	"github.com/grpc-ecosystem/grpc-gateway/v2/runtime"
 
@@ -16,25 +16,30 @@ import (
 	"google.golang.org/grpc"
 )
 
-// const MsgInterval = 3 * time.Second
-
 var runCmd = &cli.Command{
 	Name:    "run",
 	Aliases: []string{"s"},
 	Usage:   "Run the daemon",
 	Action: func(c *cli.Context) error {
-		if err := migrator.Migrate(c.Context); err != nil {
-			return err
-		}
-
-		go func() {
-			if err := grpc2.RunGRPC(rpcRegister); err != nil {
-				logger.Sugar().Errorf("fail to run grpc server: %v", err)
-			}
-		}()
-
-		return grpc2.RunGRPCGateWay(rpcGatewayRegister)
+		return action.Run(
+			c.Context,
+			run,
+			rpcRegister,
+			rpcGatewayRegister,
+			watch,
+		)
 	},
+}
+
+func run(ctx context.Context) error {
+	if err := migrator.Migrate(ctx); err != nil {
+		return err
+	}
+	return nil
+}
+
+func watch(ctx context.Context, cancel context.CancelFunc) error {
+	return nil
 }
 
 func rpcRegister(server grpc.ServiceRegistrar) error {
@@ -52,6 +57,5 @@ func rpcGatewayRegister(mux *runtime.ServeMux, endpoint string, opts []grpc.Dial
 	}
 
 	_ = apicli.Register(mux)
-
 	return nil
 }
