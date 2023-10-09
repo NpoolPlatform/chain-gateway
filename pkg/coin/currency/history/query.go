fa9dfb2f6ceb926fec9ac8d5d38f271be366977d
jakys <jakysing@163.com>
2023-10-09 09:33:46 +0000

    Fix lint

--- pkg/coin/currency/history/query.go
+++ pkg/coin/currency/history/query.go
@@ -20,7 +20,7 @@ func (h *Handler) GetCurrencies(ctx context.Context) ([]*currencymwpb.Currency, 
 	if len(h.CoinNames) > 0 {
 		conds.CoinNames = &basetypes.StringSliceVal{Op: cruder.IN, Value: h.CoinNames}
 	}
-	startAt := uint32(time.Now().Unix()) - timedef.SecondsPerDay*30
+	startAt := uint32(time.Now().Unix()) - timedef.SecondsPerDay*30 //nolint
 	if h.StartAt != nil {
 		startAt = *h.StartAt
 	}
